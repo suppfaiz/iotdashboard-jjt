3fbd5a8d95d02b5b8f6d423ee09fb47abe0666ac
ROMBENG <user@192.168.1.21>
2026-06-24 14:26:53 +0700

    style: add PHPDoc annotations to Device model to clear linter warnings

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -4,6 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $device_id
+ * @property string $name
+ * @property int $group_id
+ * @property bool $status
+ * @property string $mqtt_topic
+ * @property string|null $provisioning_code
+ * @property string|null $firmware_path
+ * @property float $voltage_multiplier
+ * @property float $current_multiplier
+ * @property float|null $monthly_budget_kwh
+ * @property float|null $monthly_budget_cost
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ */
 class Device extends Model
 {
     protected $guarded = [];
